c2e3706758a3c789ca0defb728424710cec54188
DanielC949 <danielc949949@gmail.com>
2019-11-04 18:33:22 -0800

    Update and rename classlib.js to sim.js

--- sim.js
+++ sim.js
@@ -13,7 +13,7 @@ function Spell(name, cond, effect, cd, onGCD, maxCharge, castTime, resourceGain)
 	this.dmgMod = 1;
 	this.resourceGain = resourceGain;
 }
-function Player(spellpower, atkpower, crit, haste, vers, weapon1, weapon2, class_str, gcd, maxResource, talent) {
+function Player(spellpower, atkpower, crit, haste, vers, weapon1, weapon2, class_str, talent) {
 	this.spellpower = spellpower;
 	this.atkpower = atkpower;
 	this.crit = crit;
@@ -23,10 +23,10 @@ function Player(spellpower, atkpower, crit, haste, vers, weapon1, weapon2, class
 	this.weapon2 = weapon2;
 	this.class_str = class_str;
 	this.resource = 0;
-	this.maxResource = maxResource;
+	this.maxResource = CLASSES[class_str].maxResource;
 	this.talent = talent;
 	this.buffs = new BuffList();
-	this.gcd = gcd * (1 - haste);
+	this.gcd = () => Math.max(CLASSES[class_str].gcd * (1 - haste), 0.75);
 	this.addResource = (delta) => this.resource = Math.max(Math.min(this.resource + delta, this.maxResource), 0);
 	this.dmgMod = 1 + vers;
 	this.lastCastTime = 0;
@@ -66,13 +66,6 @@ function Talent(name, onSelect) {
 	this.name = name;
 	this.onSelect = onSelect;
 	this.onDeselect = onDeselect;
-}
-function Spec(name, actList, gcd, maxResource, resourceRegen) {
-	this.name = name;
-	this.actList = actList;
-	this.gcd = gcd;
-	this.maxResource = maxResource;
-	this.resourceRegen = resourceRegen;
 }
 function ActListItem(spell, cond) {
 	this.spell = spell;
@@ -134,48 +127,72 @@ function rppmChance(ppm, lastProc, haste) {
 const TRUE = (player, target) => true;
 const NO_NEXT = function (player, target, prev){};
 const NO_EFFECT = function (player, target){};
+const CLASSES = {
+	DRUID$BALANCE: {
+		gcd: 1.5,
+		maxResource: 100,
+		useWeapon1: false,
+		useWeapon2: false
+	},
+	ROGUE$OUTLAW: {
+		gcd: 1.5,
+		maxResource: 100,
+		useWeapon1: true,
+		useWeapon2: true
+	}
+}
 
 function sim(player) {
-	const CRIT = () => Math.random() < player.crit ? 2 : 1;
+	const CRIT = () => Math.random() <= player.crit ? 2 : 1;
 	const HASTE = () => 1 - player.haste;
 	let q, target;
 
 	const SPELLS = {
 		DRUID$BALANCE: {
 			SOLAR_WRATH: new Spell("SOLAR_WRATH", TRUE, (player, target) => {
-				target.dmgTaken += 0.6 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.SOLAR_WRATH.dmgMod * CRIT();
+				let crit = CRIT(), dmg = 0.5 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.SOLAR_WRATH.dmgMod * crit;
+				target.dmgTaken += dmg;
 				player.addResource(SPELLS.DRUID$BALANCE.SOLAR_WRATH.resourceGain);
 				player.buffs.remove("STARSURGE::SOLAR_EMPOWERMENT");
+				return new InternalEvent("ACTION::SOLAR_WRATH", dmg, crit === 2, true);
 			}, 0, true, 1, 1.5, 8),
 			MOONFIRE: new Spell("MOONFIRE", TRUE, (player, target) => {
-				target.dmgTaken += 0.145 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.MOONFIRE.dmgMod * CRIT();
+				let crit = CRIT(), dmg = 0.12 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.MOONFIRE.dmgMod * crit;
+				target.dmgTaken += dmg;
 				player.addResource(SPELLS.DRUID$BALANCE.MOONFIRE.resourceGain);
 				if (!target.debuffs.has("MOONFIRE")) {
 					q.push(new Event(q.time + 2 * HASTE(), "MOONFIRE::TICK", "DOT", BUFFS.DRUID$BALANCE.MOONFIRE.onTick, BUFFS.DRUID$BALANCE.MOONFIRE.scheduleNext));
 					target.debuffs.add(BUFFS.DRUID$BALANCE.MOONFIRE);
 				}
 				BUFFS.DRUID$BALANCE.MOONFIRE.timeLastApp = q.time;
+				return new InternalEvent("ACTION::MOONFIRE", dmg, crit === 2, true);
 			}, 0, true, 1, 0, 3),
 			STARSURGE: new Spell("STARSURGE", (player, target) => player.resource >= 40, (player, target) => {
-				target.dmgTaken += 2.3 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.STARSURGE.dmgMod * CRIT();
+				let crit = CRIT(), dmg = 2.0 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.STARSURGE.dmgMod * crit;
+				target.dmgTaken += dmg;
 				player.addResource(SPELLS.DRUID$BALANCE.STARSURGE.resourceGain);
 				if (!player.buffs.has("STARSURGE::SOLAR_EMPOWERMENT")) player.buffs.add(BUFFS.DRUID$BALANCE.STARSURGE$SOLAR_EMPOWERMENT);
 				if (!player.buffs.has("STARSURGE::LUNAR_EMPOWERMENT")) player.buffs.add(BUFFS.DRUID$BALANCE.STARSURGE$LUNAR_EMPOWERMENT);
+				return new InternalEvent("ACTION::STARSURGE", dmg, crit === 2, true);
 			}, 0, true, 1, 0, -40),
 			LUNAR_STRIKE: new Spell("LUNAR_STRIKE", TRUE, (player, target) => {
-				target.dmgTaken += 0.765 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.LUNAR_STRIKE.dmgMod * CRIT();
+				let crit = CRIT(), dmg = 0.62 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.LUNAR_STRIKE.dmgMod * crit;
+				target.dmgTaken += dmg;
 				player.addResource(SPELLS.DRUID$BALANCE.LUNAR_STRIKE.resourceGain);
 				player.buffs.remove("STARSURGE::LUNAR_EMPOWERMENT");
 				if (player.buffs.has("WARRIOR_OF_ELUNE")) player.buffs.get("WARRIOR_OF_ELUNE").loseStack(player, target);
+				return new InternalEvent("ACTION::LUNAR_STRIKE", dmg, crit === 2, true);
 			}, 0, true, 1, 2.25, 12),
 			SUNFIRE: new Spell("SUNFIRE", TRUE, (player, target) => {
-				target.dmgTaken += 0.2 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.SUNFIRE.dmgMod * CRIT();
+				let crit = CRIT(), dmg = 0.17 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.SUNFIRE.dmgMod * crit;
+				target.dmgTaken += dmg;
 				player.addResource(SPELLS.DRUID$BALANCE.SUNFIRE.resourceGain);
 				if (!target.debuffs.has("SUNFIRE")) {
 					q.push(new Event(q.time + 2 * HASTE(), "SUNFIRE::TICK", "DOT", BUFFS.DRUID$BALANCE.SUNFIRE.onTick, BUFFS.DRUID$BALANCE.SUNFIRE.scheduleNext));
 					target.debuffs.add(BUFFS.DRUID$BALANCE.SUNFIRE);
 				}
 				BUFFS.DRUID$BALANCE.SUNFIRE.timeLastApp = q.time;
+				return new InternalEvent("ACTION::SUNFIRE", dmg, crit === 2, true);
 			}, 0, true, 1, 0, 3),
 			WARRIOR_OF_ELUNE: new Spell("WARRIOR_OF_ELUNE", (player, target) => {
 				return player.talent === "WARRIOR_OF_ELUNE" && q.time - SPELLS.DRUID$BALANCE.WARRIOR_OF_ELUNE.timeLastUsed >= SPELLS.DRUID$BALANCE.WARRIOR_OF_ELUNE.cd;
@@ -183,14 +200,24 @@ function sim(player) {
 				SPELLS.DRUID$BALANCE.WARRIOR_OF_ELUNE.timeLastUsed = q.time;
 				player.buffs.add(BUFFS.DRUID$BALANCE.WARRIOR_OF_ELUNE);
 				player.buffs.get("WARRIOR_OF_ELUNE").stacks = 3;
+				return new InternalEvent("ACTION::WARRIOR_OF_ELUNE", 0, false, true);
 			}, 45, true, 1, 0, 0)
+		},
+		ROGUE$OUTLAW: {
+			SINISTER_STRIKE: new Spell("SINISTER_STRIKE", (player, target) => {
+				return
+			})
 		}
 	};
 	const BUFFS = {
 		DRUID$BALANCE: {
 			MOONFIRE: new Buff("MOONFIRE", 1, NO_EFFECT, NO_EFFECT, NO_EFFECT, NO_EFFECT, (player, target) => {
-				target.dmgTaken += 0.135 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.MOONFIRE.dmgMod * CRIT();
-				if (q.time - BUFFS.DRUID$BALANCE.MOONFIRE.timeLastApp > 16) target.debuffs.remove("MOONFIRE");
+				let crit = CRIT(), dmg = 0.052 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.MOONFIRE.dmgMod * crit * ((q.time - Math.max(BUFFS.DRUID$BALANCE.MOONFIRE.timeLastTick, BUFFS.DRUID$BALANCE.MOONFIRE.timeLastApp)) / (2 * HASTE()));
+				target.dmgTaken += dmg;
+				if (q.time - BUFFS.DRUID$BALANCE.MOONFIRE.timeLastApp > 22)
+					target.debuffs.remove("MOONFIRE");
+				BUFFS.DRUID$BALANCE.MOONFIRE.timeLastTick = Math.min(q.time, BUFFS.DRUID$BALANCE.MOONFIRE.timeLastApp + 22);
+				return new InternalEvent("MOONFIRE::TICK", dmg, crit === 2, true);
 			}, (player, target, prev) => {
 				if (target.debuffs.has("MOONFIRE"))
 					q.push(new Event(prev.time + 2 * HASTE(), "MOONFIRE::TICK", "DOT", BUFFS.DRUID$BALANCE.MOONFIRE.onTick, BUFFS.DRUID$BALANCE.MOONFIRE.scheduleNext));
@@ -206,8 +233,12 @@ function sim(player) {
 				SPELLS.DRUID$BALANCE.LUNAR_STRIKE.castTime /= 0.85;
 			}, NO_EFFECT, NO_EFFECT, NO_EFFECT, NO_NEXT),
 			SUNFIRE: new Buff("SUNFIRE", 1, NO_EFFECT, NO_EFFECT, NO_EFFECT, NO_EFFECT, (player, target) => {
-				target.dmgTaken += 0.145 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.SUNFIRE.dmgMod * CRIT();
-				if (q.time - BUFFS.DRUID$BALANCE.SUNFIRE.timeLastApp > 16) target.debuffs.remove("SUNFIRE");
+				let crit = CRIT(), dmg = 0.06 * player.spellpower * player.dmgMod * SPELLS.DRUID$BALANCE.SUNFIRE.dmgMod * crit * ((q.time - Math.max(BUFFS.DRUID$BALANCE.SUNFIRE.timeLastTick, BUFFS.DRUID$BALANCE.SUNFIRE.timeLastApp)) / (2 * HASTE()));
+				target.dmgTaken += dmg;
+				if (q.time - BUFFS.DRUID$BALANCE.SUNFIRE.timeLastApp > 18)
+					target.debuffs.remove("SUNFIRE");
+				BUFFS.DRUID$BALANCE.SUNFIRE.timeLastTick = Math.min(q.time, BUFFS.DRUID$BALANCE.SUNFIRE.timeLastApp + 18);
+				return new InternalEvent("SUNFIRE::TICK", dmg, crit === 2, true);
 			}, (player, target, prev) => {
 				if (target.debuffs.has("SUNFIRE"))
 					q.push(new Event(prev.time + 2 * HASTE(), "SUNFIRE::TICK", "DOT", BUFFS.DRUID$BALANCE.SUNFIRE.onTick, BUFFS.DRUID$BALANCE.SUNFIRE.scheduleNext));
@@ -224,8 +255,8 @@ function sim(player) {
 	};
 	const ACTION_LISTS = {
 		DRUID$BALANCE: new ActList([
-			new ActListItem(SPELLS.DRUID$BALANCE.MOONFIRE, (player, target) => q.time - BUFFS.DRUID$BALANCE.MOONFIRE.timeLastApp + player.gcd >= 16),
-			new ActListItem(SPELLS.DRUID$BALANCE.SUNFIRE, (player, target) => q.time - BUFFS.DRUID$BALANCE.SUNFIRE.timeLastApp + player.gcd >= 12),
+			new ActListItem(SPELLS.DRUID$BALANCE.MOONFIRE, (player, target) => q.time - BUFFS.DRUID$BALANCE.MOONFIRE.timeLastApp + player.gcd() >= 16),
+			new ActListItem(SPELLS.DRUID$BALANCE.SUNFIRE, (player, target) => q.time - BUFFS.DRUID$BALANCE.SUNFIRE.timeLastApp + player.gcd() >= 12),
 			new ActListItem(SPELLS.DRUID$BALANCE.WARRIOR_OF_ELUNE, TRUE),
 			new ActListItem(SPELLS.DRUID$BALANCE.LUNAR_STRIKE, (player, target) => player.talent === "WARRIOR_OF_ELUNE" && player.buffs.has("WARRIOR_OF_ELUNE") && player.resource + SPELLS.DRUID$BALANCE.LUNAR_STRIKE.resourceGain <= player.maxResource),
 			new ActListItem(SPELLS.DRUID$BALANCE.STARSURGE, (player, target) => !player.buffs.has("STARSURGE::SOLAR_EMPOWERMENT") && !player.buffs.has("STARSURGE::LUNAR_EMPOWERMENT")),
@@ -265,27 +296,32 @@ function sim(player) {
 				if (e.cond(player, target)) q.push(e.event);
 
 		let open = ACTION_LISTS[player.class_str].next(player, target);
-		let openEvent = new Event(0, "ACTION::" + open.name, "PLAYER_ACTION", (player, target) => {
-				open.effect(player, target);
+		let openEvent = new Event(0, "ACTION::UNDECIDED", "PLAYER_ACTION", (player, target) => {
+				let s = ACTION_LISTS[player.class_str].next(player, target);
+				openEvent.name = s.name;
 				player.lastCastTime = open.castTime * HASTE();
+				return s.effect(player, target);
 			}, (player, target, prev) => {
-				let next = ACTION_LISTS[player.class_str].next(player, target);
-				q.push(new Event(prev.time + Math.max(player.gcd * HASTE(), player.lastCastTime * HASTE()), "ACTION::" + next.name, "PLAYER_ACTION", (player, target) => {
-					next.effect(player, target);
-					player.lastCastTime = next.castTime * HASTE();
-				}, openEvent.scheduleNext));
+				let e = new Event(prev.time + Math.max(player.gcd(), player.lastCastTime * HASTE()), "ACTION::UNDECIDED", "PLAYER_ACTION", (player, target) => {
+					let s = ACTION_LISTS[player.class_str].next(player, target);
+					e.name = s.name;
+					player.lastCastTime = s.castTime * HASTE();
+					return s.effect(player, target);
+				}, openEvent.scheduleNext)
+				q.push(e);
 		});
 		q.push(openEvent);
 
 		let last, cur, lastdmg = 0;
 		const DEBUG = log !== undefined && log !== null;
 		while ((q.time = (cur = q.pop()).time) <= limit) {
-			cur.effect(player, target);
+			let e = cur.effect(player, target);
 			cur.scheduleNext(player, target, cur);
-			if (DEBUG) {log.log(new logEvent(cur.time.toFixed(2), cur.type, cur.name, (target.dmgTaken - lastdmg).toFixed(2)))};
+			if (DEBUG && e !== undefined) {log.log(new LogEvent(q.time.toFixed(2), cur.type, cur.name, e.dmg, e.isCrit))};
 			last = cur;
 			lastdmg = target.dmgTaken;
 		}
+		if (log !== undefined && log !== null) log.time = Math.max(q.time, limit);
 		return target.dmgTaken / limit;
 	}
 }
